c192ac9520fc32689ed0637fbce26856747666a0
Daxode <dax@daxode.dk>
2024-06-03 23:22:44 +0200

    Fix performance of tree-sitter

--- Packages/com.daxode.codimation/Codimation.TreeSitter/NativeSource~/tree-sitter-unity-richtext/src/lib.rs
+++ Packages/com.daxode.codimation/Codimation.TreeSitter/NativeSource~/tree-sitter-unity-richtext/src/lib.rs
@@ -1,7 +1,6 @@
 use std::ffi::{c_char, CStr, CString};
 use tree_sitter_highlight::{Highlighter, HighlightEvent};
 use tree_sitter_highlight::HighlightConfiguration;
-use tree_sitter_highlight::HtmlRenderer;
 
 static HIGHLIGHT_NAMES: &[&str] = &[
     "attribute",
@@ -47,116 +46,98 @@ static HIGHLIGHT_COLORS: &[&str] = &[
     "#db7c77",
 ];
 
-static CLASSES: &[&str] = &[
-    "class=\"attribute\"",
-    "class=\"constant\"",
-    "class=\"comment\"",
-    "class=\"constructor\"",
-    "class=\"constant_builtin\"",
-    "class=\"function\"",
-    "class=\"function_builtin\"",
-    "class=\"keyword\"",
-    "class=\"operator\"",
-    "class=\"property\"",
-    "class=\"punctuation\"",
-    "class=\"string\"",
-    "class=\"type\"",
-    "class=\"type.builtin\"",
-    "class=\"variable\"",
-    "class=\"variable_builtin\"",
-    "class=\"variable_parameter\"",
-    "class=\"type\"",
-    "class=\"number\"",
-];
-
-#[repr(C)]
-pub struct CodeSnippets {
-    snippets: *const *const c_char,
-    snippet_count: u32,
+#[no_mangle]
+pub  unsafe extern "C" fn highlight_c_sharp_free(ptr: *const c_char) {
+    if ptr.is_null() {
+        return;
+    }
+    let _ = CString::from_raw(ptr as *mut c_char);
 }
 
+static mut C_SHARP_CONFIG: Option<HighlightConfiguration> = None;
+
 #[no_mangle]
-pub unsafe extern "C" fn highlight_c_sharp(source_raw: *const c_char) -> CodeSnippets {
-    let source = CStr::from_ptr(source_raw).to_bytes();
-    let mut c_sharp_config = HighlightConfiguration::new(
+pub unsafe extern "C" fn highlight_c_sharp_set_defaults() {
+    let mut config = HighlightConfiguration::new(
         tree_sitter_c_sharp::language(),
         "c_sharp",
         HIGHLIGHTS_QUERY,
         "",
         LOCALS_QUERY,
     ).unwrap();
-
-    c_sharp_config.configure(&HIGHLIGHT_NAMES);
-    let mut binding = Highlighter::new();
-    let highligts
-        = binding
-        .highlight(&c_sharp_config, source, None, |_| None)
-        .unwrap();
-
-    let mut renderer = HtmlRenderer::new();
-    renderer.render(highligts, source, &|attr| {
-        CLASSES[attr.0].as_bytes()
-    }).unwrap();
-
-    let raw_lines = renderer.lines()
-        .map(|x| CString::new(x).unwrap().into_raw()).collect::<Vec<_>>();
-    let len = raw_lines.len() as u32;
-    let boxed = raw_lines.into_boxed_slice();
-    return CodeSnippets {
-        snippets: Box::into_raw(boxed) as *const _,
-        snippet_count: len,
-    };
+    config.configure(&HIGHLIGHT_NAMES);
+    C_SHARP_CONFIG = Some(config);
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn highlight_c_sharp_v2(source_raw: *const c_char) -> *const c_char {
-    let highlight_query = CString::new(HIGHLIGHTS_QUERY_B).unwrap();
-    let locals_query = CString::new(LOCALS_QUERY_B).unwrap();
-    return highlight_c_sharp_v2_manual(source_raw, highlight_query.as_ptr(), locals_query.as_ptr());
-}
-#[no_mangle]
-pub unsafe extern "C" fn highlight_c_sharp_v2_manual(source_raw: *const c_char, highlight_query_raw: *const c_char, locals_query_raw: *const c_char) -> *const c_char {
-    let source = CStr::from_ptr(source_raw).to_bytes();
-    let mut c_sharp_config = HighlightConfiguration::new(
+pub unsafe extern "C" fn highlight_c_sharp_set_queries(highlight_query_raw: *const c_char, locals_query_raw: *const c_char) {
+    let mut config = HighlightConfiguration::new(
         tree_sitter_c_sharp::language(),
         "c_sharp",
         CStr::from_ptr(highlight_query_raw).to_str().unwrap(),
         "",
         CStr::from_ptr(locals_query_raw).to_str().unwrap(),
     ).unwrap();
+    config.configure(&HIGHLIGHT_NAMES);
+    C_SHARP_CONFIG = Some(config);
+}
 
-    c_sharp_config.configure(&HIGHLIGHT_NAMES);
+#[no_mangle]
+pub unsafe extern "C" fn highlight_c_sharp(source_raw: *const c_char) -> *const c_char {
+    let source = CStr::from_ptr(source_raw).to_bytes();
+    let config = C_SHARP_CONFIG.as_ref().unwrap();
+
     let mut binding = Highlighter::new();
     let highlighter
         = binding
-        .highlight(&c_sharp_config, source, None, |_| None)
+        .highlight(config, source, None, |_| None)
         .unwrap();
 
-    // let mut highlights = Vec::new();
     let mut str_builder = String::new();
-    str_builder.push_str("<color=#000000>");
+    let mut escaped_temp_str = String::new();
+    // str_builder.push_str("<color=#000000>");
     for event in highlighter {
         match event {
             Ok(HighlightEvent::HighlightStart(s)) => {
-                // highlights.push(s);
-                // println!("Highlight start: {}", HIGHLIGHT_NAMES[s.0]);
                 str_builder.push_str(&format!("<color={}>", HIGHLIGHT_COLORS[s.0]));
             }
             Ok(HighlightEvent::HighlightEnd) => {
-                // highlights.pop();
-                // println!("Highlight end");
                 str_builder.push_str("</color>");
             }
             Ok(HighlightEvent::Source { start, end }) => {
                 let source = std::str::from_utf8(&source[start..end]).unwrap();
-                // let highlights = highlights.iter().map(|x| HIGHLIGHT_NAMES[x.0]).collect::<Vec<_>>();
-                // println!("Source: `{}` - `{}`", source, highlights.join(", "));
-                str_builder.push_str(source.replace("<", "&lt;").replace(">", "<b></b>><b></b>").replace("&lt;", "<b></b><<b></b>").as_str());
+                escaped_temp_str.clear();
+                escaped_temp_str.reserve(source.len());
+
+                // way one
+                let mut current_read = 0;
+                let mut consecutive = 0;
+                for c in source.as_bytes() {
+                    match c {
+                        b'<' => {
+                            escaped_temp_str.push_str(&source[current_read..current_read +consecutive]);
+                            escaped_temp_str.push_str("<b></b><<b></b>");
+                            current_read += consecutive;
+                            consecutive = 0;
+                        },
+                        b'>' =>{
+                            escaped_temp_str.push_str(&source[current_read..current_read +consecutive]);
+                            escaped_temp_str.push_str("<b></b>><b></b>");
+                            current_read += consecutive;
+                            consecutive = 0;
+                        }
+                        _ => consecutive += 1,
+                    }
+                }
+                escaped_temp_str.push_str(&source[current_read..current_read +consecutive]);
+
+
+                str_builder.push_str(escaped_temp_str.as_str());
             }
             Err(a) => println!("Error: {:?}", Err::<(), tree_sitter_highlight::Error>(a)),
         }
     }
-    str_builder.push_str("</color>");
+    // str_builder.push_str("</color>");
 
     return CString::new(str_builder).unwrap().into_raw();
 }
@@ -164,17 +145,15 @@ pub unsafe extern "C" fn highlight_c_sharp_v2_manual(source_raw: *const c_char, 
 /// The syntax highlighting query for this language.
 pub const HIGHLIGHTS_QUERY: &str = include_str!("../queries/highlights.scm");
 pub const LOCALS_QUERY: &str = include_str!("../queries/locals.scm");
-pub const HIGHLIGHTS_QUERY_B: &[u8] = include_bytes!("../queries/highlights.scm");
-pub const LOCALS_QUERY_B: &[u8] = include_bytes!("../queries/locals.scm");
 
 #[cfg(test)]
 mod tests {
     use std::ffi::CStr;
-    use std::{slice};
 
     #[test]
     fn test_can_load_grammar() {
         unsafe {
+            super::highlight_c_sharp_set_defaults();
             let res = super::highlight_c_sharp(c"public class DoesItWork : MonoBehaviour
 {
     void Start()
@@ -188,16 +167,15 @@ mod tests {
         var strBuilder = new System.Text.StringBuilder();
     }
 }".as_ptr());
-            slice::from_raw_parts(res.snippets, res.snippet_count as usize).iter().for_each(|&x| {
-                let source = CStr::from_ptr(x).to_str().unwrap();
-                println!("{}", source);
-            });
+            println!("{}", CStr::from_ptr(res).to_str().unwrap());
         }
     }
 
     #[test]
     fn manual_highlight() {
-        let source = c"public class DoesItWork : MonoBehaviour
+        unsafe {
+            super::highlight_c_sharp_set_defaults();
+            let source = c"public class DoesItWork : MonoBehaviour
 {
     void Start()
     {
@@ -210,8 +188,7 @@ mod tests {
         var str_builder = new System.Text.StringBuilder();
     }
 }".as_ptr();
-        unsafe {
-            let default = super::highlight_c_sharp_v2(source);
+            let default = super::highlight_c_sharp(source);
             println!("{}", CStr::from_ptr(default).to_str().unwrap());
         }
     }
--- Packages/com.daxode.codimation/Codimation.TreeSitter/TreeSitter.cs
+++ Packages/com.daxode.codimation/Codimation.TreeSitter/TreeSitter.cs
@@ -1,83 +1,49 @@
-﻿// #define USING_V1
-
-using System;
+﻿using System;
 using System.IO;
 using System.Runtime.InteropServices;
+using UnityEditor;
 
 namespace Codimation.TreeSitter
 {
     public static class TreeSitterUtility
     {
         [DllImport("tree_sitter_unity_richtext")]
-        extern static CodeSnippets highlight_c_sharp(IntPtr sourceCodeCString);
-        [DllImport("tree_sitter_unity_richtext")]
-        extern static IntPtr highlight_c_sharp_v2(IntPtr sourceCodeCString);
+        extern static IntPtr highlight_c_sharp(IntPtr sourceCodeCString);
     
         [DllImport("tree_sitter_unity_richtext")]
-        extern static IntPtr highlight_c_sharp_v2_manual(IntPtr sourceCodeCString, IntPtr highlightQueryRaw, IntPtr localsQueryRaw);
-    
-        public static string Highlight(string sourceCode)
-        {
-#if USING_V1
-        var sourceCodeCString = Marshal.StringToHGlobalAnsi(sourceCode);
-        var codeSnippets = highlight_c_sharp(sourceCodeCString);
-        Marshal.FreeHGlobal(sourceCodeCString);
-        var strBuilder = new StringBuilder();
-        for (int i = 0; i < codeSnippets.snippet_count; i++)
-        {
-            unsafe
-            {
-                var str = Marshal.PtrToStringAnsi((IntPtr)codeSnippets.snippets[i]);
-                // strBuilder.Append($"{i + 1}: ");
-                strBuilder.Append(str
-                    .Replace("<span class=\"attribute\">", "<color=#9a7fed>")
-                    .Replace("<span class=\"constant\">", "<color=#aa44ff>")
-                    .Replace("<span class=\"comment\">", "<color=#84c26a>")
-                    .Replace("<span class=\"constructor\">", "<color=#38A256>")
-                    .Replace("<span class=\"constant_builtin\">", "<color=#518fc7>")
-                    .Replace("<span class=\"function\">", "<color=#38A256>")
-                    .Replace("<span class=\"function_builtin\">", "<color=#38A256>")
-                    .Replace("<span class=\"keyword\">", "<color=#518fc7>")
-                    .Replace("<span class=\"operator\">", "<color=white>")
-                    .Replace("<span class=\"property\">", "<color=#42a1c5>")
-                    .Replace("<span class=\"punctuation\">", "<color=white>")
-                    .Replace("<span class=\"string\">", "<color=#c9a26d>")
-                    .Replace("<span class=\"type\">", "<color=#9a7fed>")
-                    .Replace("<span class=\"type.builtin\">", "<color=#518fc7>")
-                    .Replace("<span class=\"variable\">", "<color=#bdbdbd>")
-                    .Replace("<span class=\"variable_builtin\">", "<color=#bdbdbd>")
-                    .Replace("<span class=\"variable_parameter\">", "<color=#bdbdbd>")
-                    .Replace("<span class=\"number\">", "<color=#db7c77>")
-                    .Replace("</span>", "</color>")
-                    .Replace("&quot;", "\"")
-                    .Replace("&amp;", "&")
-                    .Replace("&#39;", "'")
-                    .Replace("&lt;", "<b></b><<b></b>")
-                    .Replace("&gt;", "<b></b>><b></b>")
-                );
-            }
-        }
+        extern static IntPtr highlight_c_sharp_set_queries(IntPtr highlightQueryRaw, IntPtr localsQueryRaw);
+        [DllImport("tree_sitter_unity_richtext")]
+        extern static IntPtr highlight_c_sharp_set_defaults();
         
-        return strBuilder.ToString();
-#else
-            var sourceCodeCString = Marshal.StringToHGlobalAnsi(sourceCode);
+        [DllImport("tree_sitter_unity_richtext")]
+        extern static void highlight_c_sharp_free(IntPtr highlightedCodeCString);
+
+        [InitializeOnLoadMethod]
+        public static void Setup()
+        {
             var highlightQueryReader = new StreamReader(@"Packages\com.daxode.codimation\Codimation.TreeSitter\NativeSource~\tree-sitter-unity-richtext\queries\highlights.scm");
             var highlightQuery = highlightQueryReader.ReadToEnd();
             var highlightQueryCString = Marshal.StringToHGlobalAnsi(highlightQuery);
             var localsQueryReader = new StreamReader(@"Packages\com.daxode.codimation\Codimation.TreeSitter\NativeSource~\tree-sitter-unity-richtext\queries\locals.scm");
             var localsQuery = localsQueryReader.ReadToEnd();
             var localsQueryCString = Marshal.StringToHGlobalAnsi(localsQuery);
-
-            // return $"{highlightQuery} {localsQuery}";
-            var highlightedCodeCString = highlight_c_sharp_v2_manual(sourceCodeCString, highlightQueryCString, localsQueryCString);
-            Marshal.FreeHGlobal(sourceCodeCString);
+            
+            highlight_c_sharp_set_queries(highlightQueryCString, localsQueryCString);
+            
             Marshal.FreeHGlobal(highlightQueryCString);
             Marshal.FreeHGlobal(localsQueryCString);
             highlightQueryReader.Close();
             localsQueryReader.Close();
+        }
+        
+        public static string Highlight(string sourceCode)
+        {
+            var sourceCodeCString = Marshal.StringToHGlobalAnsi(sourceCode);
+            var highlightedCodeCString = highlight_c_sharp(sourceCodeCString);
+            Marshal.FreeHGlobal(sourceCodeCString);
             var highlightedCode = Marshal.PtrToStringAnsi(highlightedCodeCString);
+            highlight_c_sharp_free(highlightedCodeCString);
             return highlightedCode;
-#endif
         }
 
         public static string HighlightWithLineNumbers(string sourceCode)
@@ -103,11 +69,4 @@ namespace Codimation.TreeSitter
             return stringBuilder.ToString();
         }
     }
-}
-
-[StructLayout(LayoutKind.Sequential)]
-unsafe struct CodeSnippets
-{
-    public char** snippets;
-    public uint snippet_count;
 }
